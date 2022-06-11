a14f6b1c9197ad0484f56a0af4527342b0cae5eb
Nikita <bigmama93@mail.ru>
2022-06-11 14:51:45 +0300

    step 13 of 13

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ import telegram
 env = Env()
 env.read_env()
 
+CHAT_ID = env('CHAT_ID')
+TG_TOKEN = env('TG_TOKEN')
+TOKEN = env('DEVMAN_TOKEN')
+
 
 async def main(*messages):
-    bot = telegram.Bot(env('TG_TOKEN'))
+    bot = telegram.Bot(TG_TOKEN)
     async with bot:
-        await bot.send_message(text=f'У вас проверили работу "{messages[0]}" \n\n {messages[1]}', chat_id=305151573)
+        await bot.send_message(text=f'У вас проверили работу "{messages[0]}" \n\n {messages[1]}', chat_id=CHAT_ID)
 
 
 def get_notification():
-    headers = {'Authorization': env('DEVMAN_TOKEN')}
+    headers = {'Authorization': TOKEN}
     while True:
         try:
             response = requests.get('https://dvmn.org/api/long_polling/', headers=headers)
             if response.json()['status'] == 'found':
-                print(response.json())
-                print('отправил сообщение')
                 messages_1 = response.json()['new_attempts'][0]['lesson_title']
                 if response.json()['new_attempts'][0]['is_negative']:
                     messages_2 = 'К сожалению, в работе нашлись ошибки.'
@@ -30,11 +32,9 @@ def get_notification():
                     messages_2 = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
                     asyncio.run(main(messages_1, messages_2))
             elif response.json()['status'] == 'timeout':
-                print('новый респонс')
                 timestamp = {'timestamp': response.json()['timestamp_to_request']}
                 new_response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=timestamp)
                 if new_response.json()['status'] == 'found':
-                    print(response.json())
                     asyncio.run(main())
         except requests.exceptions.ReadTimeout:
             print('время истекло')
