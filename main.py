4a652de02518430c55379ec030ca5ef5af2b87e9
Nikita <bigmama93@mail.ru>
2022-06-14 13:43:42 +0300

    fixed mistakes for codereview_1

--- main.py
+++ main.py
@@ -5,50 +5,49 @@ import asyncio
 import telegram
 
 
-
-async def send_message(*messages):
-    bot = telegram.Bot(TG_TOKEN)
+async def send_message(tg_token, chat_id, *messages):
+    bot = telegram.Bot(tg_token)
     async with bot:
         await bot.send_message(text=f'У вас проверили работу "{messages[0]}" \n\n {messages[1]} \n'
-                                    f'{messages[2]}', chat_id=CHAT_ID)
+                                    f'{messages[2]}', chat_id=chat_id)
 
 
-def get_notification():
-    headers = {'Authorization': TOKEN}
+def main():
+    env = Env()
+    env.read_env()
+    chat_id = env('TG_CHAT_ID')
+    tg_token = env('TG_TOKEN')
+    token = env('DEVMAN_TOKEN')
+    headers = {'Authorization': token}
     params = None
     while True:
         try:
             response = requests.get(
                 'https://dvmn.org/api/long_polling/',
                 headers=headers,
-                params=params
+                params=params,
+
             )
-            if response.json()['status'] == 'timeout':
-                params["timestamp"] = response.json()["timestamp_to_request"]
+            response.raise_for_status()
+            serialized_response = response.json()
+            if serialized_response['status'] == 'timeout':
+                params = {'timestamp': serialized_response["timestamp_to_request"]}
                 continue
-            params = {'timestamp': response.json()['last_attempt_timestamp']}
-            message_1 = response.json()['new_attempts'][0]['lesson_title']
-            if response.json()['new_attempts'][0]['is_negative']:
-                message_2 = 'К сожалению, в работе нашлись ошибки.'
+            params = {'timestamp': serialized_response['last_attempt_timestamp']}
+            message_title = serialized_response['new_attempts'][0]['lesson_title']
+            if serialized_response['new_attempts'][0]['is_negative']:
+                message_result = 'К сожалению, в работе нашлись ошибки.'
             else:
-                message_2 = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
-            message_3 = response.json()['new_attempts'][0]['lesson_url']
-            asyncio.run(send_message(message_1, message_2, message_3))
+                message_result = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
+            message_link = serialized_response['new_attempts'][0]['lesson_url']
+            asyncio.run(send_message(tg_token, chat_id, message_title, message_result, message_link))
         except requests.exceptions.ReadTimeout:
-            print('время истекло')
             continue
         except requests.exceptions.ConnectionError:
             print('Интернет отключился,а я нет')
-            sleep(5)
 
 
 if __name__ == '__main__':
-    env = Env()
-    env.read_env()
-    CHAT_ID = env('TG_CHAT_ID')
-    TG_TOKEN = env('TG_TOKEN')
-    TOKEN = env('DEVMAN_TOKEN')
-
-    get_notification()
+    main()
 
 
