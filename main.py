97be4119c36d3d447b98a4818a3c12ad6d3a068a
Nikita <bigmama93@mail.ru>
2022-06-25 17:52:17 +0300

    add log entry in tg

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import requests
 from time import sleep
 from environs import Env
-import asyncio
 import telegram
 import logging
 
+logger = logging.getLogger(__file__)
 
-async def send_message(tg_token, chat_id, *messages):
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_token, chat_id):
+        super().__init__()
+        self.tg_bot = telegram.Bot(tg_token)
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def send_message(tg_token, chat_id, *messages):
     bot = telegram.Bot(tg_token)
-    async with bot:
-        await bot.send_message(text=f'У вас проверили работу "{messages[0]}" \n\n {messages[1]} \n'
-                                    f'{messages[2]}', chat_id=chat_id)
+    bot.send_message(text=f'У вас проверили работу "{messages[0]}" \n\n {messages[1]} \n'
+                     f'{messages[2]}', chat_id=chat_id)
 
 
 def main():
@@ -19,6 +30,10 @@ def main():
     chat_id = env('TG_CHAT_ID')
     tg_token = env('TG_TOKEN')
     token = env('DEVMAN_TOKEN')
+    logging.basicConfig(level=logging.ERROR)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(TelegramLogsHandler(tg_token, chat_id))
+    logger.info('бот запущен')
     headers = {'Authorization': token}
     params = None
     while True:
@@ -42,16 +57,15 @@ def main():
             else:
                 message_result = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
             message_link = result_reviews['new_attempts'][0]['lesson_url']
-            asyncio.run(send_message(tg_token, chat_id, message_title, message_result, message_link))
-        except requests.exceptions.ReadTimeout:
+            send_message(tg_token, chat_id, message_title, message_result, message_link)
+        except requests.exceptions.ReadTimeout as err:
+            logger.info(err)
             continue
-        except requests.exceptions.ConnectionError:
-            print('Интернет отключился,а я нет')
+        except requests.exceptions.ConnectionError as err:
+            logger.info(err)
             sleep(5)
 
 
+
 if __name__ == '__main__':
-    logging.warning('Бот запущен')
     main()
-
-
